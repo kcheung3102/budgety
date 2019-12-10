b45a6aff4d44b028dc5234afdeb44adc2a0e3e3d
kcheung3102 <kcheung3102@gmail.com>
2019-12-09 23:43:27 -0500

    updating budget

--- app.js
+++ app.js
@@ -179,7 +179,12 @@ var budgetController = (function() {
         document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
         document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
         document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        
+        if(obj.percentage > 0) {
+          document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        } else {
+          document.querySelector(DOMstrings.percentageLabel).textContent = "--";
+        }
       },
 
       getDOMstrings: function() {
@@ -244,7 +249,7 @@ var budgetController = (function() {
       UICtrl.clearField();
   
       //Calculate and Updates the Budget
-      updateBudget
+      updateBudget();
   
       }
   
@@ -253,6 +258,14 @@ var budgetController = (function() {
   return {
     init: function() {
       console.log('App has started');
+      //sets everything to 0 in the beginning 
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+
+      });
       setupEventListeners();
     }
   }
